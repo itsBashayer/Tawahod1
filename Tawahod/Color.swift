fe14316ba5d3c376aea8132d80d6c29c8055fc3b
monasaad <monaaa.saad@yahoo.com>
2024-09-28 21:45:11 +0300

    added a changable avatar state

---
+++ Tawahod/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  Tawahod
+//
+//  Created by Mona on 25/03/1446 AH.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkGrey = Color(red: 0.3607843137254902, green: 0.3607843137254902, blue: 0.3607843137254902)
+    static let lightGrey = Color(red: 0.9607843137254902, green: 0.9607843137254902, blue: 0.9686274509803922)
+    static let lightPink = Color(red: 1, green: 0.9098039215686274, blue: 0.9529411764705882)
+    static let lightRed = Color(red: 1, green: 0.8980392156862745, blue: 0.9019607843137255)
+    static let lightPurple = Color(red: 0.9607843137254902, green: 0.9529411764705882, blue: 0.996078431372549)
+    static let lightGreen = Color(red: 0.9333333333333333, green: 0.9686274509803922, blue: 0.9490196078431372) // RGB
+    //static let lightBlue = Color(red: 1, green: 0.8980392156862745, blue: 0.9019607843137255) // RGB
+    static let lightYellow = Color(red: 1, green: 0.9647058823529412, blue: 0.8352941176470589) // RGB
+
+
+
+
+}
--- Tawahod/ContentView.swift
+++ Tawahod/ContentView.swift
@@ -8,14 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    let items: [(imageName: String, frameColor: Color)] = [
+        ("avatar1", Color.lightYellow),
+        ("avatar2", Color.lightGreen),
+        ("avatar3", Color.lightYellow),
+        ("avatar4", Color.lightGreen),
+        ("avatar5", Color.lightYellow),
+        ("avatar6", Color.lightPurple),
+        ("avatar7", Color.lightPurple),
+        ("avatar8", Color.lightPurple),
+        ("avatar1", Color.lightGreen),
+        ("avatar1", Color.lightYellow),
+        ("avatar2", Color.lightGreen),
+        ("avatar3", Color.lightYellow),
+        ("avatar4", Color.lightGreen),
+        ("avatar5", Color.lightYellow),
+        ("avatar6", Color.lightPurple),
+        ("avatar7", Color.lightPurple),
+        ("avatar8", Color.lightPurple),
+        ("avatar1", Color.lightGreen),
+    ]
+
+    @State private var selectedAvatar: String = "avatar3"  // State variable for selected avatar
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Circle().foregroundColor(Color.lightGrey)
+                    .frame(width: 150, height: 150)
+                Image(selectedAvatar).resizable()
+                    .scaledToFit()  // Maintain aspect ratio
+                    .frame(width: 120, height: 120)
+            }
+
+            Text("اختر شخصيتك!")
+                .foregroundColor(Color.darkGrey)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            ScrollView {
+                LazyVGrid(
+                    columns: [GridItem(.adaptive(minimum: 90))], spacing: 10
+                ) {
+                    ForEach(Array(items.enumerated()), id: \.offset) {
+                        index, item in
+                        AvatarFrame(
+                            avatarImg: item.imageName,
+                            frameColor: item.frameColor
+                        )
+                        .onTapGesture {
+                            selectedAvatar = item.imageName
+                        }
+                    }  //Use id: \.offset: This uses the index (offset) as the identifier, ensuring that each item is unique even if the image names are the same.
+
+                }
+                .padding(30)
+            }
+
         }
-        .padding()
+        .padding(.top, 20)
+
+    }
+}
+
+struct AvatarFrame: View {
+    var avatarImg: String
+    var frameColor: Color
+
+    var body: some View {
+        ZStack {
+            Circle().foregroundColor(frameColor)
+                .frame(width: 100, height: 100)
+            Image(avatarImg).resizable()
+                .scaledToFit()  // Maintain aspect ratio
+                .frame(width: 80, height: 80)
+        }
     }
 }
 
