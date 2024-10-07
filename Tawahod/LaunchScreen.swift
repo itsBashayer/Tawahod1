2fae212612d1f9fede680e3415bd280bf756904a
AfrahSaud36 <afrahsaud36@gmail.com>
2024-10-07 12:15:35 +0300

    LaunchPage

---
+++ Tawahod/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreen.swift
+//  Tawahod
+//
+//  Created by Afrah Alharbi on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreen()
+}
